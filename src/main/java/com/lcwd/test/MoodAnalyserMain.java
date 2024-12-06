cd56d3f18986a160949474bb918acf2c06c5d0b4
tanmaylohakare <tanmaylohakare820@gmail.com>
2024-12-06 14:36:48 +0530

    WelCome Message

---
+++ src/main/java/com/lcwd/test/MoodAnalyserMain.java
@@ -0,0 +1,9 @@
+package com.lcwd.test;
+
+public class MoodAnalyserMain {
+
+    public static void main(String[] args) {
+        System.out.println("WelCome to Mood Analyser Problem ");
+
+    }
+}
