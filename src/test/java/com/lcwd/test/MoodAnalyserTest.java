c650e33f87fabd15b578fcbda3d7a391f1755314
tanmaylohakare <tanmaylohakare820@gmail.com>
2024-12-09 11:09:14 +0530

    When Mood is Happy Returns Happy

--- src/test/java/com/lcwd/test/MoodAnalyserTest.java
+++ src/test/java/com/lcwd/test/MoodAnalyserTest.java
@@ -12,6 +12,11 @@ public class MoodAnalyserTest {
         Assert.assertEquals("SAD",m1.AnalyseMood("I am Sad"));
 
     }
+    @Test
+    public void When_Message_Is_Happy_Should_Return_Happy(){
 
+        MoodAnalyser m1=new MoodAnalyser();
+        Assert.assertEquals("HAPPY",m1.AnalyseMood("I am Happy"));
+    }
 
 }
